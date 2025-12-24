eb566263e16660b0a2e5053ea6275e22bb838c2e
windi fikriyansyah <diwin321@gmail.com>
2025-12-25 01:48:32 +0700

    d

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\AdminController;
@@ -6,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
 
-Route::get('/', [AuthController::class, 'showLoginForm'])->name('login');
+Route::get('/', [AuthController::class, 'showLoginForm']);
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::get('/register', [AuthController::class, 'create'])->name('register');
 Route::post('/simpan-register', [AuthController::class, 'register'])->name('simpan_register');
@@ -18,11 +19,8 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/Academic', [StudentController::class, 'academy'])->name('student.academy');
     Route::get('/Financial', [StudentController::class, 'financial'])->name('student.financial');
     Route::get('/Financial-Summary', [StudentController::class, 'summary'])->name('student.summary');
-
 });
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/student/dashboard', [AuthController::class, 'showDashboardstudent'])->name('student.dashboard')->middleware('auth', 'role:student');
-
-
